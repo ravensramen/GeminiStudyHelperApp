3167dc3dd6ef0d5a19a16a8e3ea15412f18d15c2
Yourname <sydnee.boothby@wsu.edu>
2026-02-01 16:02:21 -0800

    streamlit app working

--- src/test_app.py
+++ src/test_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
+from google import genai
 
-st.title("Hello World!")
+from google.genai import types
+from PIL import Image
+
+import io
+
+#loads my API key from the streamlit framework
+api_key = st.secrets["GEMINI_API_KEY"]
+
+#initialize client with API key
+client = genai.Client(api_key=api_key)
+
+#title of streamlit site
+st.title("Welcome to Image Generator")
+
+#text box prompt
+prompt = st.text_input("Enter an image prompt")
+
+#testing button 
+if st.button("Click me"):
+    st.write("Somethiing was clicked")
