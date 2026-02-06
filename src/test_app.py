3cd26462ab4ce656546fc94ad71912b7c83c2c21
Yourname <sydnee.boothby@wsu.edu>
2026-02-06 07:37:51 -0800

    Making a flashcard app, add export and gemini generation

--- src/test_app.py
+++ src/test_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import json
 from google import genai
-
 from google.genai import types
-from PIL import Image
 
-import io
+#set page title on tab
+st.set_page_config(page_title="Flashcard App", layout="centered")
 
 #loads my API key from the streamlit framework
 api_key = st.secrets["GEMINI_API_KEY"]
@@ -13,53 +13,33 @@ api_key = st.secrets["GEMINI_API_KEY"]
 client = genai.Client(api_key=api_key)
 
 #title of streamlit site
-st.title("Welcome, test out the newest Gemini Model")
+st.title("Voice to Study Notes App")
 
-#test audio features of gemini
-audio_file = st.audio_input("Input a recording")
+#write main program prompt
+st.write("Talk about what you learned in lecture today, Gemini can turn it into structured"
+" notes and flashcards that you can study with!"
+)
 
-#Program response if button clicked
-if st.button("Generate Response"):  
-    if audio_file: #If audio received, pass prompt to gemini model
-        with st.spinner("Generating response..."): #load spinning wheel
-            try: #use try block in case API doesn't work
-               
-                # Convert audio bytes to Part object with mime type
-                audio_part = types.Part(
-                    inline_data=types.Blob(
-                        mime_type="audio/wav",
-                        data=audio_file.getvalue()
-                    )
+#take in audio file from user
+audio_file = st.audio_input("Record your topic")
+
+
+#Button interactivity 
+if st.button("Generate Notes & Flashcards"):  
+    if not audio_file: #if button was clicked without input
+        st.warning("Record some audio first!.")
+else:
+    with st.spinner("Generating..."):
+        try:
+            audio_part = types.Part( #convert audio to a format gemini can evaluate
+                inline_data=types.Blob(
+                    mime_type="audio/wav",
+                    data=audio_file.getvalue()
                 )
-                
-                response = client.models.generate_content( #call generate content function based on prompt
-                    model="gemini-3-flash-preview",
-                    contents=[audio_part],
-                    config=types.GenerateContentConfig(
-                        thinking_config=types.ThinkingConfig(
-                            thinking_level=types.ThinkingLevel.HIGH
-                        )
-                    )
                 )
+            #call gemini and generate content
 
-                #testing image generation with gemini based on audio description
-                image_prompt = response.text  # Use audio description as image prompt
-                response2 = client.models.generate_content(
-                    model="gemini-3-pro-image-preview",
-                    contents=[image_prompt]
-                )
 
-                
-                # display the response of audio prompt
-                st.success("Response: ")
-                st.write(response.text) #write response from gemini response
-
-                #display generated image
-                if response2.candidates[0].content.parts:
-                    image_data = response2.candidates[0].content.parts[0].inline_data.data
-                    st.image(image_data, caption="Generated Image from Audio")
-
-            except Exception as e: #if any ai model fails, run this block
-                st.error(f"Error generating response: {str(e)}") #str(e) details error specs
-    else:
-        st.warning("Please enter a prompt first")
+    #catch if error occured and print error info
+        except Exception as e:
+            st.error(f"Error generating from audio: {e}")
